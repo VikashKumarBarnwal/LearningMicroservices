b8ac7a6ddb8123082baced3e026bbb820476ac53
vikash kumar <vku_jaina@yahoo.co.in>
2024-03-31 17:01:20 +0530

    Microservice Saga pattern programming

---
+++ SagaDesignPattern/CodeBase/OrderService/src/test/java/com/vks/saga/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vks.saga.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
